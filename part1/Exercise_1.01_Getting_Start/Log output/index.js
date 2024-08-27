36c860ffc107b4fcf5d3a8414f8e24d6edbfe1ad
zhengwei <a932327192@gmail.com>
2024-08-26 20:20:07 -0400

    1.1 first deploy

---
+++ part1/Exercise_1.01_Getting_Start/Log output/index.js
@@ -0,0 +1,16 @@
+// generate random string with length
+function randomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const str = randomString(30);
+// print string to console
+console.log(str);
+setInterval(() => {
+    console.log(str);
+}, 5000);
