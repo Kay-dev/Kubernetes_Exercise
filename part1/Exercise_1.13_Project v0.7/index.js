39395b98119adf9a183d1ca0c807a7b5c4e30e19
zhengwei <a932327192@gmail.com>
2024-09-07 21:50:30 -0400

    exercise 1.12, 1.13

---
+++ part1/Exercise_1.13_Project v0.7/index.js
@@ -0,0 +1,59 @@
+const Koa = require('koa')
+const Router = require('@koa/router');
+const app = new Koa()
+const port = process.env.PORT || 3000;
+const router = new Router();
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const imgPath = '/usr/src/app/files/1200.jpg';
+const imgUrl = 'https://picsum.photos/1200';
+
+var readImage = () => {
+    if (!fs.existsSync(imgPath)) {
+        getAndSaveImage();
+        return fs.readFileSync(imgPath);
+    }
+    return fs.readFileSync(imgPath);
+}
+
+var getAndSaveImage = () => {
+    try {
+        axios.get(imgUrl, { responseType: 'stream' })
+            .then(response => {
+                response.data.pipe(fs.createWriteStream(imgPath));
+            });
+    } catch (err) {
+        console.error("Error while downloading image: ", error);
+    }
+}
+
+setInterval(getAndSaveImage, 60 * 60 * 1000);
+
+router.get('/', (ctx) => {
+    try {
+        ctx.status = 200;
+        ctx.body = `<img src="data:image/jpg;base64,${readImage().toString('base64')}" style="width: 400px; height: auto" />
+            <div>
+                <input type="text" maxlength="140"></>
+                <input type="button" value="create TODO" />    
+            </div>
+            <div>
+                <ul>
+                    <li>TODO 1</li>
+                    <li>TODO 2</li>
+                </ul>
+            </div>
+        `;
+    } catch (error) {
+        ctx.status = 500;
+        ctx.body = "Error serving image";
+        console.error("Error serving image:", error);
+    }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+    console.log(`Server started in port ${port}...`)
+})
